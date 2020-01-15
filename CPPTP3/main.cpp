f06fc49f23cba111690931dfa4e17a27554d51f8
Sophie Crowley <crowleso@tcd.ie>
2020-01-15 17:32:46 +0100

    Update main.cpp

--- CPPTP3/main.cpp
+++ CPPTP3/main.cpp
@@ -22,7 +22,8 @@ int main(int argc, char** argv)
     else {
         string graphFileName = "";
         bool includeImages = true;
-        int time = -1;
+        int startTime = 0;
+        int endTime = 24;
         for (int i = 0; i < argc-1; ++i) {
             if (string(argv[i]) == "-g") { 
                 if((i+1)< argc-1 && string(argv[i+1]).at(0)!='-') {
@@ -38,13 +39,28 @@ int main(int argc, char** argv)
             else if (string(argv[i]) == "-t") {
                 if((i+1)< argc-1 && atoi(argv[i+1])>=0 && atoi(argv[i+1])<24 
                 && string(argv[i+1]).at(0)!='-') {
-                    time = atoi(argv[++i]);
+                    startTime = atoi(argv[++i]);
+                    endTime = startTime+1;
                 }
                 else {
                     printf("No hour was given, time will not be accounted for");
                 }    
             }
         } 
+        // make stats map
+        // make graph map if making one
+        // open stream for log file
+        // do loop
+        // get time
+        // if : time is between (or equal to) start and end time then do loop stuff
+        // get file type
+        // if includeImages = true || its not an image file  then do loop stuff
+        // get referrer and hit url strings
+        // add 'hit' url to map or increment it if its already there
+        // if doing graph, then add get and referrer to map
+        // or increment w referrer if already there
+        // call stats afficher method
+        // etc. 
     }
 } 
                         

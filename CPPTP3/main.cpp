278320022f5dd4bc55d11f7a6ac839fe73aa54a6
Sophie Crowley <crowleso@tcd.ie>
2020-01-15 15:54:45 +0100

    Create main.cpp

---
+++ CPPTP3/main.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+                           main  -  description
+                             -------------------
+    début                : $15/01/2020$
+    copyright            : (C) $2019$ par $Sophie Crowley, Zakaria Nassreddine, Mohamed EDDAHABI 
+    e-mail               : $sophie.crowley@insa-lyon.fr$
+                           $zakaria.nassreddine@insa-lyon.fr$
+                           $mohamed.eddhabi@insa-lyon.fr$
+*******************************************************************************/
+
+using namespace std;
+
+int main() {
+}

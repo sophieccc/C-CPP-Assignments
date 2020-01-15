a836e4a63fb284ccd9d14b0fa7c3abc3773582d9
Sophie Crowley <crowleso@tcd.ie>
2020-01-15 17:43:16 +0100

    Added log file loop

--- CPPTP3/main.cpp
+++ CPPTP3/main.cpp
@@ -11,56 +11,64 @@
 #include <iostream> 
 #include <string> 
 #include <cstdlib>
+#include <map> 
 
 using namespace std; 
   
 int main(int argc, char** argv) 
 { 
     string fileName = argv[(argc-1)];
-    if(argc==1) 
-        printf("\nNo log file name was passed"); 
-    else {
-        string graphFileName = "";
-        bool includeImages = true;
-        int startTime = 0;
-        int endTime = 24;
-        for (int i = 0; i < argc-1; ++i) {
-            if (string(argv[i]) == "-g") { 
-                if((i+1)< argc-1 && string(argv[i+1]).at(0)!='-') {
-                    graphFileName =  string(argv[++i]);
-                }
-                else {
-                    printf("No graph file name was passed, graph will not be made");
-                }  
-            }
-            else if (string(argv[i]) == "-e") { 
-                includeImages = false;     
-            }        
-            else if (string(argv[i]) == "-t") {
-                if((i+1)< argc-1 && atoi(argv[i+1])>=0 && atoi(argv[i+1])<24 
-                && string(argv[i+1]).at(0)!='-') {
-                    startTime = atoi(argv[++i]);
-                    endTime = startTime+1;
-                }
-                else {
-                    printf("No hour was given, time will not be accounted for");
-                }    
-            }
-        } 
-        // make stats map
-        // make graph map if making one
-        // open stream for log file
-        // do loop
-        // get time
-        // if : time is between (or equal to) start and end time then do loop stuff
-        // get file type
-        // if includeImages = true || its not an image file  then do loop stuff
-        // get referrer and hit url strings
-        // add 'hit' url to map or increment it if its already there
-        // if doing graph, then add get and referrer to map
-        // or increment w referrer if already there
-        // call stats afficher method
-        // etc. 
+    if(argc==1) {
+        printf("\nNo log file name was passed");
+        return 0; 
     }
+    string graphFileName = "";
+    bool includeImages = true;
+    int startTime = 0;
+    int endTime = 24;
+    for (int i = 0; i < argc-1; ++i) {
+        if (string(argv[i]) == "-g") { 
+            if((i+1)< argc-1 && string(argv[i+1]).at(0)!='-') {
+                graphFileName =  string(argv[++i]);
+            }
+            else {
+                printf("No graph file name was passed, graph will not be made");
+            }  
+        }
+        else if (string(argv[i]) == "-e") { 
+            includeImages = false;     
+        }        
+        else if (string(argv[i]) == "-t") {
+            if((i+1)< argc-1 && atoi(argv[i+1])>=0 && atoi(argv[i+1])<24 
+            && string(argv[i+1]).at(0)!='-') {
+                startTime = atoi(argv[++i]);
+                endTime = startTime+1;
+            }
+            else {
+                printf("No hour was given, time will not be accounted for");
+            }    
+        }
+    } 
+
+    map<string, int> stats;
+    if(graphFileName != "") {
+        map<string, (map<string, int>)> graph;
+    }
+    ifstream file(filename);
+    if(file.good()) {
+        string input;
+        while(getline(file,input))
+        {
+            // get time
+            // if : time is between (or equal to) start and end time then do loop stuff
+            // get file type
+            // if includeImages = true || its not an image file  then do loop stuff
+            // get referrer and hit url strings
+            // add 'hit' url to map or increment it if its already there
+            // if doing graph, then add get and referrer to map
+            // or increment w referrer if already there
+        }
+        // call stats afficher method etc.
+    return 0;
 } 
                         

0d88162355cb28d81c294fd4ae408a2f847bb006
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-01-25 12:39:40 +0100

    test

--- CPPTP3/main.cpp
+++ CPPTP3/main.cpp
@@ -90,8 +90,8 @@ int main(int argc, char** argv)
             }
         }
     }
-    statistics *statsMap=new statistics(stats);
-    statsMap->printTopX(5);
+    //statistics *statsMap=new statistics(stats);
+    //statsMap->printTopX(5);
     graph *graphMap=new graph(graphInput);
     graphMap->writeGraph("hi.dot");
     return 0;
